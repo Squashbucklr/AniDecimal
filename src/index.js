748f2d72e30f5f5d4b57c53ef6ba1192d1745d8f
Squashbucklr <2jbwar22@gmail.com>
2021-03-21 15:09:15 -0400

    use /anime for anime

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const app = express();
 
 app.get('/', require('./page/home.js'));
-app.get('/css/:alusername/:malusername/scores.css', cors(), require('./gen/scorecss.js').gen);
+app.get('/anime/:alusername/:malusername/scores.css', cors(), require('./gen/scorecss.js').gen);
+app.get('/anime/:alusername/:malusername/data.json', cors(), require('./gen/scorecss.js').data);
 
 app.listen('3136', function() {
     console.log('ready!');
